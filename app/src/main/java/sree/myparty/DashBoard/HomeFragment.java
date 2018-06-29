0dad66c3693be11751027f6096be7dce33686a7a
srikanthk <ksreekanthyadav@gmail.com>
2018-06-29 13:49:23 +0530

    test

--- app/src/main/java/sree/myparty/DashBoard/HomeFragment.java
+++ app/src/main/java/sree/myparty/DashBoard/HomeFragment.java
@@ -1,11 +1,13 @@
 package sree.myparty.DashBoard;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import sree.myparty.R;
 
@@ -15,10 +17,21 @@ import sree.myparty.R;
 
 public class HomeFragment extends Fragment {
 
+     ImageView db_op1;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-       
-        return inflater.inflate(R.layout.home_fragment,null,false);
+
+        View v =inflater.inflate(R.layout.home_fragment,null,false);
+        db_op1 = (ImageView)v.findViewById(R.id.db_op1);
+        db_op1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog d = new Dialog(getActivity());
+                d.setContentView(R.layout.id_card);
+                d.show();
+            }
+        });
+        return v;
     }
 }
