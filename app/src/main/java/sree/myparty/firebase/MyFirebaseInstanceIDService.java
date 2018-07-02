815d2004cd1d5cda8e11c5956d08b70f12a83130
srikanthk <ksreekanthyadav@gmail.com>
2018-07-02 19:50:15 +0530

    Firbase Service

--- app/src/main/java/sree/myparty/firebase/MyFirebaseInstanceIDService.java
+++ app/src/main/java/sree/myparty/firebase/MyFirebaseInstanceIDService.java
@@ -21,7 +21,7 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
-        // If you want to send messages to this application instance or
+        //If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
         //sendRegistrationToServer(refreshedToken);
