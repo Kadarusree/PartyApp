67799792d46c759e4a8ccf91e1c7018503febdd0
shridhars <smetraskar@gmail.com>
2018-07-06 17:20:12 +0530

    card update

--- app/src/main/java/sree/myparty/pojos/UserDetailPojo.java
+++ app/src/main/java/sree/myparty/pojos/UserDetailPojo.java
@@ -18,10 +18,10 @@ public class UserDetailPojo {
 
 
 
-    String voter_id,mobile_number,reg_id,both_number,user_name,state_name,pc_name,ac_name, fcm_id;
+    String voter_id,mobile_number,reg_id,both_number,user_name,state_name,pc_name,ac_name, fcm_id,profile_url,qr_url;
     int points;
 
-    public UserDetailPojo(String voter_id, String mobile_number, String reg_id, String both_number, String user_name, String state_name, String pc_name, String ac_name, int points,String fcm_id) {
+    public UserDetailPojo(String voter_id, String mobile_number, String reg_id, String both_number, String user_name, String state_name, String pc_name, String ac_name, int points,String fcm_id,String profile_url,String qr_url) {
         this.voter_id = voter_id;
         this.mobile_number = mobile_number;
         this.reg_id = reg_id;
@@ -32,6 +32,8 @@ public class UserDetailPojo {
         this.ac_name = ac_name;
         this.points = points;
         this.fcm_id=fcm_id;
+        this.profile_url=profile_url;
+        this.qr_url=qr_url;
     }
 
     public String getVoter_id() {
